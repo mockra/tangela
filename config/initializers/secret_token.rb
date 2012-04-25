9c5485e8d6d70754b416d1779aab9b94f02ad091
David Ratajczak <david@mockra.com>
2012-04-25 15:37:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tangela::Application.config.secret_token = '274e40e8ec4caa02b240b5b05cbd9c5e5a1dcfc517db84580cbeff89b27a2112a60f0afbd104de8c418d9e11fa21c9098cdfc090a923e056757419f0c4895bb5'
