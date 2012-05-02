103ac51b56480ddb8e4c8e7c02fe94d288ac5f59
David Ratajczak <david@mockra.com>
2012-05-01 17:41:29 -0700

    added resource/value format validation

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -2,9 +2,13 @@ class Resource < ActiveRecord::Base
 
   attr_accessible :type, :value
 
+  link_regex = /\Ahttp|https|www|(:\/\/){0,1}\.{0,1}[a-z0-9-]{1,}\.{1}.*/i
+  twitter_regex = /\A@[A-Z0-9_]*/i
+
   belongs_to :user
   belongs_to :project
 
   validates_presence_of :type, :value, :user_id, :project_id
+  validates :value, format: { with: Regexp.union( link_regex, twitter_regex ) }
 
 end
--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -10,4 +10,31 @@ describe Resource do
   it { should belong_to :user }
   it { should belong_to :project }
 
+  describe 'value format' do
+    before do
+      @resource = Resource.new
+      @resource.user_id = 1
+      @resource.project_id = 1
+      @resource.type = 'twitter'
+    end
+    describe 'failure' do
+      it 'should not accept a single word' do
+        @resource.value = 'string'
+        @resource.should_not be_valid
+      end
+    end
+
+    describe 'success' do
+      it 'should accept a twitter handle' do
+        @resource.value = '@Mockra_'
+        @resource.should be_valid
+      end
+
+      it 'should accept a link' do
+        @resource.value = 'www.google.com'
+        @resource.should be_valid
+      end
+    end
+  end
+
 end
