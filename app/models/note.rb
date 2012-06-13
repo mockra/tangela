fc4567b825322aece5f8c1dd2727e97501999bc8
David Ratajczak <david@mockra.com>
2012-06-13 12:38:24 -0700

    Updated notes.top scope to filter completed notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -6,7 +6,7 @@ class Note < ActiveRecord::Base
   belongs_to :project
 
   scope :sorted, order('completed_at ASC').order('position')
-  scope :top, order('updated_at DESC').limit(5)
+  scope :top, where('completed_at IS NULL').order('updated_at DESC').limit(5)
   scope :completed, where('completed_at IS NOT NULL')
 
   validates_presence_of :content, :user_id, :project_id
